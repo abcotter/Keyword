39eaa01f0b970a36693f76992f2a6a9173bb9b5e
Abigail Cotter <abigail.cotter@gmail.com>
2020-04-23 17:23:58 -0400

    finished text file upload

---
+++ vue-cli/src/components/Keywords.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="Keywords">
+  <h1>{{ body }}
+  </h1>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'Keywords',
+    props: {
+        body: {
+            type: String
+        }
+  }
+  }
+
+</script>
+
+<style>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ vue-cli/src/components/Upload.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="Upload">
+    <div class="dropbox">
+      <input type="file" @change="loadTextFromFile" class= "input-file" @load = "text = $event">
+      <p>
+        Drag your file(s) here to begin<br> or click to browse
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'Upload',
+    methods: {
+        loadTextFromFile(ev) {
+        const file = ev.target.files[0];
+        const reader = new FileReader();
+
+        reader.onload = e => this.$emit("load", e.target.result);
+        reader.readAsText(file);
+        }
+    }
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .dropbox {
+        outline: 2px dashed grey; /* the dash box */
+        outline-offset: -10px;
+        background: lightcyan;
+        color: dimgray;
+        padding: 10px 10px;
+        min-height: 100px; /* minimum height */
+        position: relative;
+        cursor: pointer;
+    }
+
+    .input-file {
+        opacity: 0; /* invisible but it's there! */
+        width: 100%;
+        height: 200px;
+        position: absolute;
+        cursor: pointer;
+    }
+
+    .dropbox:hover {
+        background: lightblue; /* when mouse over to the drop zone, change color */
+    }
+
+    .dropbox p {
+        font-size: 1.2em;
+        text-align: center;
+        padding: 50px 0;
+    }
+</style>
